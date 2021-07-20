efcc324b788a7fe90d49a866a3a6beaa03775817
Dylan Howard <dylanhoward0424@outlook.com>
2021-07-20 14:55:27 -0500

    initial commit

--- client.js
+++ client.js
@@ -41,3 +41,47 @@ const employees = [
 // Ask questions when you don't.
 
 console.log( employees );
+
+function bonusInformation(employees){
+  console.log("Running bonusCalculator");
+  for (let employee of employees){
+    let employeeBonus = {
+      name: employee.name,
+      bonusPercentage: bonusCalculator(employee) * 100, 
+      totalCompensation: (bonusCalculator(employee) + 1) * employee.annualSalary,
+    }
+  }
+}
+console.log();
+
+function bonusCalculator(employee) {
+  let reviewBonus = 0;
+  switch (employee.reviewRating){
+    case 2: reviewBonus = employee.annualSalary;
+      break;
+    case 3: reviewBonus = employee.annualSalary*0.04;
+      break;
+    case 4: reviewBonus = employee.annualSalary*0.06;
+      break;
+    case 5: reviewBonus = employee.annualSalary*0.10;
+      break;
+  } //end switch
+  if (employee.employeeNumber.toString().length === 4){
+    reviewBonus += employee.annualSalary*0.05;
+  } //end if
+  if (employee.annualSalary > 65000){
+    reviewBonus -= reviewBonus*0.01;
+  } //end if
+  if (reviewBonus > (employee.annualSalary*0.13)){
+    reviewBonus = (employee.annualSalary*0.13);
+  }else if (reviewBonus < 0){
+    reviewBonus = 0;
+  } //end if-else
+  let reviewPercent = reviewBonus/employee.annualSalary;
+  return reviewPercent;
+} //end bonusCalculator
+
+function totalCompensationCalculator(employee){
+  bonusCalculator(employee) // returns string '9%'
+}
+console.log('testing Bonus Calculator should be .09', bonusCalculator(employees[0]));
